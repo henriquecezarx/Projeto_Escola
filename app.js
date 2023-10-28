22264c24a3f9bcc0e31749ec4f53f45a673a549f
Henrique Cézar <henriquec.xavier2000@gmail.com>
2023-10-28 17:29:15 -0300

    JWT Update

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.post('/criar', (req, res) => {
                             classe: req.body.classe,
                             senha: hashedPassword,
                         }).save().then(() => {
-                            const token = jwt.sign({userId: 1}, SECRET, {expiresIn: 5000})
+                            const token = jwt.sign({userId: 1}, SECRET, {expiresIn: '1h'})
                             const successMessage = 'Usuário Cadastrado com Sucesso'
                             res.render('entrar', {success_msg: successMessage, token})
                             console.log(token)
